0863187fef1a77c5c080de34536496695826e426
mccartneyb <brian.mccartney2@gmail.com>
2022-06-22 09:29:23 -0400

    Implemented list of URLs when leagues span multiple pages. Added .gitignore for logo image files
    MLB and NCAAF show logos on multiple pages, which all had unique urls. Using a for each loop, each url can be passed into the function. Added Logos/* to gitignore so that image files are not added to Git.

--- Download_Logos.py
+++ Download_Logos.py
@@ -15,9 +15,20 @@ def Get_Logos(url, sport):
 
 
 NFL_url = 'https://www.sportslogos.net/teams/list_by_league/7/National_Football_League/NFL/logos/'
-MLB_AL_url = 'https://www.sportslogos.net/teams/list_by_league/53/American_League/AL/logos/'
-MLB_NL_url = 'https://www.sportslogos.net/teams/list_by_league/54/National_League/NL/logos/'
+MLB_urls = ['https://www.sportslogos.net/teams/list_by_league/53/American_League/AL/logos/'
+    , 'https://www.sportslogos.net/teams/list_by_league/54/National_League/NL/logos/']
+CFB_urls = ['https://www.sportslogos.net/teams/list_by_league/30/NCAA_Division_I_a-c/NCAA_a-c/logos/'
+    , 'https://www.sportslogos.net/teams/list_by_league/31/NCAA_Division_I_d-h/NCAA_d-h/logos/'
+    , 'https://www.sportslogos.net/teams/list_by_league/32/NCAA_Division_I_i-m/NCAA_i-m/logos/'
+    , 'https://www.sportslogos.net/teams/list_by_league/33/NCAA_Division_I_n-r/NCAA_n-r/logos/'
+    , 'https://www.sportslogos.net/teams/list_by_league/34/NCAA_Division_I_s-t/NCAA_s-t/logos/'
+    , 'https://www.sportslogos.net/teams/list_by_league/35/NCAA_Division_I_u-z/NCAA_u-z/logos/']
 
-Get_Logos(NFL_url, 'Football')
-Get_Logos(MLB_NL_url, 'Baseball_NL')
-Get_Logos(MLB_AL_url, 'Baseball_AL')
+# Get Logos
+Get_Logos(NFL_url, 'NFL')
+
+for i, url in enumerate(MLB_urls):
+    Get_Logos(url, f'MLB_{i}')
+
+for i, url in enumerate(CFB_urls):
+    Get_Logos(url, f'NCAAF_{i}')
