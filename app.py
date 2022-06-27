9dc1ada564bd4c58fa75b54a8999a7292b0503db
mccartneyb <brian.mccartney2@gmail.com>
2022-06-27 16:24:13 -0400

    Commented out all the debug print lines. Created standardized dictionary to pass to flask template

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 import get_schedules
 app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.errorhandler(404)
 def page_not_found(error):
--- get_schedules.py
+++ get_schedules.py
@@ -7,35 +7,50 @@ from sportsipy.ncaaf.teams import Teams
 def get_browns_schedule(year):
     browns_schedule = nfl.Schedule('CLE', year)
     nfl_games = []
+
     for game in browns_schedule:
         #print(vars(game))
-        nfl_games.append(game)
-        if game.location == 'Home':
-            print(f'{game.datetime.strftime("%m/%d")}  {game.opponent_abbr} vs CLE')
-        else:
-            print(f'{game.datetime.strftime("%m/%d")}  CLE @ {game.opponent_abbr}')
+        this_game = {'date': game.date,
+                     'opponent': game.opponent_name,
+                     'location': game.location,
+                     'result': game.result}
+
+        nfl_games.append(this_game)
+        # if game.location == 'Home':
+        #     print(f'{game.datetime.strftime("%m/%d")}  {game.opponent_abbr} vs CLE')
+        # else:
+        #     print(f'{game.datetime.strftime("%m/%d")}  CLE @ {game.opponent_abbr}')
     return nfl_games
 
 def get_indians_schedule(year):
     indians_schedule = mlb.Schedule('CLE', year)
     mlb_games = []
     for game in indians_schedule:
-        mlb_games.append(game)
-        if game.location == 'Home':
-            print(f'{game.datetime.strftime("%m/%d")}  {game.opponent_abbr} vs CLE')
-        else:
-            print(f'{game.datetime.strftime("%m/%d")}  CLE @ {game.opponent_abbr}')
+        this_game = {'date': game.date,
+                     'opponent': game.opponent_abbr,
+                     'location': game.location,
+                     'result': game.result}
+        mlb_games.append(this_game)
+        # if game.location == 'Home':
+        #     print(f'{game.datetime.strftime("%m/%d")}  {game.opponent_abbr} vs CLE')
+        # else:
+        #     print(f'{game.datetime.strftime("%m/%d")}  CLE @ {game.opponent_abbr}')
     return mlb_games
+
 def get_buckeyes_schedule(year):
     teams = Teams()
     OSU = teams('OHIO-STATE')
     ncaaf_games = []
     buckeyes_schedule = ncaaf.Schedule('OHIO-STATE', year)
     for game in buckeyes_schedule:
-        ncaaf_games.append(game)
+        this_game = {'date': game.date,
+                     'opponent': game.opponent_name,
+                     'location': game.location,
+                     'result': game.result}
+        ncaaf_games.append(this_game)
         game_time = parser.parse(game.date)
-        if game.location == 'Home':
-            print(f'{game_time.strftime("%m/%d")} {game.time} {game.opponent_name} vs {OSU.name}')
-        else:
-            print(f'{game_time.strftime("%m/%d")} {game.time} {OSU.name} @ {game.opponent_name}')
+        # if game.location == 'Home':
+        #     print(f'{game_time.strftime("%m/%d")} {game.time} {game.opponent_name} vs {OSU.name}')
+        # else:
+        #     print(f'{game_time.strftime("%m/%d")} {game.time} {OSU.name} @ {game.opponent_name}')
     return ncaaf_games
