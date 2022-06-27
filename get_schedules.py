daf6b1e6b5bbe84463b49c49398e5ea587a9a564
mccartneyb <brian.mccartney2@gmail.com>
2022-06-27 16:48:17 -0400

    code clean up, print out all properties of each league game for future reference

--- get_schedules.py
+++ get_schedules.py
@@ -3,13 +3,16 @@ import sportsipy.nfl.schedule as nfl
 import sportsipy.ncaaf.schedule as ncaaf
 from dateutil import parser
 from sportsipy.ncaaf.teams import Teams
+from pprint import pprint
+
 
 def get_browns_schedule(year):
     browns_schedule = nfl.Schedule('CLE', year)
     nfl_games = []
-
+    print(f'\nNFL\n')
+    pprint(dir(browns_schedule._games[0]))
     for game in browns_schedule:
-        #print(vars(game))
+        # print(vars(game))
         this_game = {'date': game.date,
                      'opponent': game.opponent_name,
                      'location': game.location,
@@ -22,9 +25,12 @@ def get_browns_schedule(year):
         #     print(f'{game.datetime.strftime("%m/%d")}  CLE @ {game.opponent_abbr}')
     return nfl_games
 
+
 def get_indians_schedule(year):
     indians_schedule = mlb.Schedule('CLE', year)
     mlb_games = []
+    print(f'\nMLB\n')
+    pprint(dir(indians_schedule._games[0]))
     for game in indians_schedule:
         this_game = {'date': game.date,
                      'opponent': game.opponent_abbr,
@@ -37,20 +43,25 @@ def get_indians_schedule(year):
         #     print(f'{game.datetime.strftime("%m/%d")}  CLE @ {game.opponent_abbr}')
     return mlb_games
 
+
 def get_buckeyes_schedule(year):
-    teams = Teams()
-    OSU = teams('OHIO-STATE')
-    ncaaf_games = []
     buckeyes_schedule = ncaaf.Schedule('OHIO-STATE', year)
+    ncaaf_games = []
+    print(f'\nNCAAF\n')
+    pprint(dir(buckeyes_schedule._games[0]))
     for game in buckeyes_schedule:
         this_game = {'date': game.date,
                      'opponent': game.opponent_name,
                      'location': game.location,
                      'result': game.result}
         ncaaf_games.append(this_game)
-        game_time = parser.parse(game.date)
+        # game_time = parser.parse(game.date)
         # if game.location == 'Home':
-        #     print(f'{game_time.strftime("%m/%d")} {game.time} {game.opponent_name} vs {OSU.name}')
+        #     print(f'{game_time.strftime("%m/%d")} {game.time} {game.opponent_name} vs OSU')
         # else:
-        #     print(f'{game_time.strftime("%m/%d")} {game.time} {OSU.name} @ {game.opponent_name}')
+        #     print(f'{game_time.strftime("%m/%d")} {game.time} OSU @ {game.opponent_name}')
     return ncaaf_games
+
+get_browns_schedule(2022)
+get_buckeyes_schedule(2021)
+get_indians_schedule(2022)
